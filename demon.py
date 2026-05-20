1e2a87638d0a4753ac673249afbe87a4f253c8fb
make199221 <make199221@gmail.com>
2026-05-20 13:10:11 +0500

    auto update photo demon_s1

--- demon.py
+++ demon.py
@@ -2,7 +2,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 SEASON_PHOTOS = {
     "demon_main": "AgACAgIAAxkBAAIEQWoMqEvL0IyczdkVqcK38CHMk41YAAI2G2sbmRJpSJG1TU1nsqCIAQADAgADeQADOwQ",
-    "demon_s1": "PHOTO_ID",
+    "demon_s1": "AgACAgIAAxkBAAIEZmoNabxAxAr6kQ6RJEmjgou3ltDBAAK0HmsbmRJxSMD7XiW0MKCEAQADAgADeQADOwQ",
     "demon_s2": "PHOTO_ID",
     "demon_s3": "PHOTO_ID",
     "demon_s4": "PHOTO_ID",
@@ -317,7 +317,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 SEASON_PHOTOS = {
     "demon_main": "AgACAgIAAxkBAAIEQWoMqEvL0IyczdkVqcK38CHMk41YAAI2G2sbmRJpSJG1TU1nsqCIAQADAgADeQADOwQ",
-    "demon_s1": "PHOTO_ID",
+    "demon_s1": "AgACAgIAAxkBAAIEZmoNabxAxAr6kQ6RJEmjgou3ltDBAAK0HmsbmRJxSMD7XiW0MKCEAQADAgADeQADOwQ",
     "demon_s2": "PHOTO_ID",
     "demon_s3": "PHOTO_ID",
     "demon_s4": "PHOTO_ID",
